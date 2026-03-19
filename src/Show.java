631f05ade51a05c456a2d9e760c6820fbefc9591
Liliia Gorokhova <lil@MacBook-Pro-Lil.local>
2026-03-19 15:45:08 +0700

    add new class

---
+++ src/Show.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Show {
+    private String title;
+    private int duration;
+    private Director director;
+    private  ArrayList<Actor> listOfActors;
+
+    public Show(String title, int duration, Director director, ArrayList<Actor> listOfActors) {
+        this.title = title;
+        this.duration = duration;
+        this.director = director;
+        this.listOfActors = listOfActors;
+    }
+
+    public void printListOfActors() {
+        System.out.println("Список актеров учавствующие в спектакле " + title + ": ");
+
+        for (Actor actor : listOfActors) {
+            System.out.println(actor);
+        }
+    }
+
+    public void addActorInShow(Actor newActor){
+        for (Actor actor : listOfActors) {
+            if (actor.equals(newActor)) {
+                System.out.println("Этот актер уже учавствует в спектакле!");
+                return;
+            }
+        }
+
+       listOfActors.add(newActor);
+        System.out.println("Актер " + newActor.getName() + " " + newActor.getSurname() + " добавлен в спектакль " + title);
+    }
+
+    public void replaceActorInShow(Actor replaceableActor,Actor newActor){
+        boolean isRemove = listOfActors.remove(replaceableActor);
+
+        if (!isRemove){
+            System.out.println("Aктера "+ replaceableActor.getName() + " " + replaceableActor.getSurname() + " нет в этом спектакле!");
+            return;
+        }
+
+        listOfActors.add(newActor);
+    }
+}
---
+++ src/Theatre.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Theatre {
+
+    public static void main(String[] args) {
+        ArrayList<Actor> listOfActors = new ArrayList<>();
+        ArrayList<Actor> listOfActorsBallet = new ArrayList<>();
+        ArrayList<Actor> listOfActorsOpera = new ArrayList<>();
+
+        String textBallet = "Буря стихает. К берегу острова подплывает Конрад. На его руках Али, потерявший сознание. Конрад выбирается на берег и падает без чувств.";
+        String textOpera = "С о л д а т ы\n" +
+                            "Оживленною толпою все снует и бежит.\n" +
+                            "Странный народ! Куда спешит?\n" +
+                            "Странный народ! Куда спешит?\n" +
+                            "\n" +
+                            "М о р а л е с\n" +
+                            "Чтоб скорей проходило время, любим мы подчас\n" +
+                            "Курить, болтать и над прохожим подшутить не раз.\n" +
+                            "Оживленною толпою все снует и бежит.";
+
+        Actor actor1 = new Actor("Василий","Кузнецов",Gender.MALE,178);
+        Actor actor2 = new Actor("Андрей","Антонов",Gender.MALE,175);
+        Actor actor3 = new Actor("Екатерина","Марченко",Gender.FEMALE,167);
+
+        Director directorOpera = new Director("Антон","Андреев",Gender.MALE,2);
+        Director director = new Director("Валерий","Николаев",Gender.MALE,2);
+
+        Person musicAuthor = new Person("Мария","Сверидова",Gender.FEMALE);
+        Person choreographer = new Person("Анна","Маркова",Gender.FEMALE);
+
+        Ballet ballet = new Ballet("Корсар",135,director,listOfActorsBallet,musicAuthor,textBallet,choreographer);
+        Opera opera = new Opera("Кармен",200,directorOpera,listOfActorsOpera,musicAuthor,textOpera,2);
+        Show show = new Show("Одиссея",120,director,listOfActors);
+
+        ballet.addActorInShow(actor1);
+        ballet.addActorInShow(actor3);
+
+        opera.addActorInShow(actor1);
+        opera.addActorInShow(actor2);
+
+        show.addActorInShow(actor1);
+        show.addActorInShow(actor2);
+        show.addActorInShow(actor3);
+
+        ballet.printListOfActors();
+        opera.printListOfActors();
+        show.printListOfActors();
+
+        ballet.replaceActorInShow(actor3,actor2);
+        ballet.printListOfActors();
+
+        opera.replaceActorInShow(actor3,actor2);
+
+        opera.printLibrettoText();
+        System.out.println();
+        ballet.printLibrettoText();
+
+    }
+}
